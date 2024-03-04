d14aa5c7a3483f69e638be68e1e67097d48f7cc1
Lovis <lovis_@systemli.org>
2024-03-04 17:53:26 +0100

    CSS, add tailwindcss

--- frontend/src/components/BookList.tsx
+++ frontend/src/components/BookList.tsx
@@ -7,16 +7,6 @@ import axios from "axios";
 export default function BookList() {
     const [bookList, setBookList] = useState<BookListResource | null>();
 
-    // async function load() {
-    //     try {
-    //         const i = await getBookList();
-    //         setBookList(i);
-    //     } catch (err: any) {
-    //         setBookList(null);
-    //     }
-    // }
-
-    // useEffect( () => {load();}, []);
 
     useEffect(() => {
         axios.get(`${API_URL}/books`).then((response) => {
@@ -30,8 +20,9 @@ export default function BookList() {
         return (<ul>
             {bookList!.map( (book) => (
                 <li>
-                    <p>{book.title}</p>
+                    <p className="text-3xl" >{book.title}</p>
                     <p>by {book.author} Year: {book.year?.toString()}</p>
+                    <button>Details</button>
                 </li>
             ))}
         </ul> )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.tsx", 
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
