da5088d15421d6e689c7d0ab9ed24c2569c457c0
Lovis <lovis_@systemli.org>
2024-03-17 10:08:38 +0100

    React, BookDetail style without card background and add more attributes

--- frontend/src/components/BookDetail.tsx
+++ frontend/src/components/BookDetail.tsx
@@ -7,7 +7,7 @@ import cover from '../static/img/cover.jpg';
 
 /**
  * BookDetail is a JSX component which gets the book id as parameter in the url and gets the book from the backend_api.
- * @returns a jsx element with a detailed overview over the book attributes.
+ * @returns a jsx element with a detailed overview over the book attributes in two columns (left: cover image, right: book details).
  */
 export default function BookDetail() {
     const [book, setBook] = React.useState<BookResource | null>();
@@ -28,20 +28,22 @@ export default function BookDetail() {
     } else {
         return (
             <div className="books">
-                <div className="book" id="bookdetail">
+                <div className="bookdetail">
                     <img id="bookdetail-cover" alt={`Cover of ${book.title}`} src={cover} />
                     <div aria-label="Book details." id="bookdetail-text">
                         <div id="bookdetail-header">
-                            <h1>{book.title}</h1>
+                            <h1>{book.title} ({book.year?.toString()})</h1>
                             <Link to={`/book/${book.id}/edit/`}>
                                     <button>Edit Book</button>
                             </Link>
                         </div>
-                        <p>{book.author}</p>
-                        <p>{book.year?.toString()}</p>
-                        <p>{book.edition}</p>
+                        <p id="bookdetail-author">{`by ${book.author}`}</p>
+                        <p>{`Genre: ${book.genre}`}</p>
+                        <p>{`Edition: ${book.edition}`}</p>
+                        <p>{`Publisher: ${book.publisher} (${book.address})`}</p>
                         <p>{book.location}</p>
                         <p>{book.borrowed_from}</p>
+                        <p>{book.comments}</p>
                     </div>
                     
                     
